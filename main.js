926a5c6f73c826217a4f138db062f8c199acd192
Jeffery Jones <yreffej94@gmail.com>
2020-02-06 11:31:50 -0800

    Instantiated PageHeader instance in main.js, added pageHeader parameter to constructor in App class

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const tableEl = document.getElementById("grade-table");
+const pageHeaderEl = document.getElementsByTagName("header")[0];
 const gradeTable = new GradeTable(tableEl);
-const app = new App(gradeTable);
+const pageHeader = new PageHeader(pageHeaderEl);
+const app = new App(gradeTable, pageHeader);
 app.start();
