a5d9457a9891d1a35d82a312f66e3866fd6400a3
Jeffery Jones <yreffej94@gmail.com>
2020-02-06 12:05:58 -0800

    Added dynamic grade average changes to the page element in the PageHeader class

--- components/page-header.js
+++ components/page-header.js
@@ -1,9 +1,11 @@
 class PageHeader {
   constructor(headerElement) {
     this.headerElement = headerElement;
+    this.displayAverageElements = document.getElementsByClassName("display-average");
   }
 
   updateAverage(newAverage) {
+    this.displayAverageElements[0].textContent = newAverage;
     console.log(newAverage);
   }
 }
