e81e73b159c809c92aae25f36fa7edf4916c4570
Jeffery Jones <yreffej94@gmail.com>
2020-02-05 23:41:19 -0800

    Created App class and instantiated in main.js - 404 error at url for api

---
+++ components/app.js
@@ -0,0 +1,29 @@
+class App {
+  constructor(){
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  getGrades() {
+    console.log("getGrades running");
+    $.ajax({
+      url: "http://sgt.lfzprototypes.com",
+      method: "GET",
+      path: "/api/grades",
+      headers: {
+        "X-Access-Token":"JF3KZ7oy"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    });
+    console.log("getGrades has been run");
+  }
+  start() {
+    this.getGrades();
+  }
+  handleGetGradesError(error) {
+    console.error("Error", error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log("Grades", grades);
+  }
+}
